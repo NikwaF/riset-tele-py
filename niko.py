45bc611c2e8bebcee82ad20ca156e261128297c1
niko wahyu fitrianto <nikonabilah@gmail.com>
2022-11-17 01:44:20 +0700

    send message without get_entity first

--- niko.py
+++ niko.py
@@ -1,15 +1,26 @@
-from telethon.tl.types import InputPhoneContact, User
-from telethon import TelegramClient
+from telethon.tl.types import InputPhoneContact
+from telethon import TelegramClient, errors
 import asyncio
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
+
+async def def_test():
+    return os.getenv("TELEGRAM_API_ID") 
 
 class SenderTelegram:
-    api_id = 123123123
-    api_hash = '' #bot
-    session = ''
+    api_id = int(os.getenv("TELEGRAM_API_ID"))
+    api_hash = os.getenv("TELEGRAM_API_HASH") 
+    session = os.getenv("TELEGRAM_SESSION") 
+    target = ""
 
-    def __init__(self, target):
+    def __init__(self):
         self.client = TelegramClient(self.session, api_id=self.api_id, api_hash=self.api_hash)
-        self.target = target
+
+    async def initTelegramClient(self):
+        # await self.client.start()
+        await self.client.connect() 
 
     async def getId(self):
         listEntity = []
@@ -53,22 +64,70 @@ class SenderTelegram:
         return listEntity
     
     async def sendMessage(self, entity, pesan):
-        await self.client.send_message(entity=entity, message=pesan)
+        try: 
+            await self.client.send_message(entity=entity, message=pesan)
+        except ValueError as a:
+            raise Exception("username not found")
+        except errors.rpcerrorlist.FloodWaitError as a:
+            raise Exception("telegram api has been limited")
+        except Exception as a:
+            raise Exception("something wrong")
+
 
 async def get_entity(entity):
-    terus = 1
-    while True:
-        for i in range(1,16):
-            api_id = 999999 
-            api_hash = '' #bot    
-            session = ''    
-            client = TelegramClient(session, api_id=api_id, api_hash=api_hash)
-            await client.start()
-            await client.connect()
-            ent = User()
-            test =  await client.send_message(ent, f"test {terus}")
-            await client.disconnect()
-            terus = terus+1
+    # terus = 1
+    # while True:
+        # for i in range(1,16):
+    api_id = int(os.getenv("TELEGRAM_API_ID"))
+    api_hash = os.getenv("TELEGRAM_API_HASH") 
+    session = os.getenv("TELEGRAM_SESSION") 
+    client = TelegramClient(session, api_id=api_id, api_hash=api_hash)
+    await client.start()
+    await client.connect()
+    try:
+        tes = 1
+        while True:
+            test =  await client.send_message(entity,f"test {tes}")
+            tes = tes+1
+        # test = await client.send_file(entity,'./test.CSV')
+        
+        # # if int(test.peer_id.user_id) != 5627950104:
+        # test =  await client.delete_messages(entity,test.id)   
+    except ValueError as a:
+        print(type(a))
+        print(a)
+    except errors.rpcerrorlist.FloodWaitError as a:
+        print("limit")
+    except Exception as a:
+        print(type(a))
+        print(a)
+    await client.disconnect()
+    # terus = terus+1
+
+async def get_entity2(entity):
+    # terus = 1
+    # while True:
+        # for i in range(1,16):
+    api_id = int(os.getenv("TELEGRAM_API_ID"))
+    api_hash = os.getenv("TELEGRAM_API_HASH") 
+    session = os.getenv("TELEGRAM_SESSION") 
+    client = TelegramClient(session, api_id=api_id, api_hash=api_hash)
+    await client.start()
+    await client.connect()
+    try:
+        while True:
+            test =  await client.send_message(entity,"ttt")
+            print(test)
+    except ValueError as a:
+        print(type(a))
+        print(a)
+    except Exception as a:
+        print(a)
+    await client.disconnect()
+    # terus = terus+1
 
 if __name__ == "__main__":
-    asyncio.run(get_entity("nikohxh"))
+    # print(halo.peer_id.user_id)
+    #1094219920 tidak bisa harus pake username dulu
+    asyncio.run(get_entity("nko738827"))
+    # asyncio.run(get_entity2("Nikohxh"))
--- server.py
+++ server.py
@@ -1,64 +1,74 @@
 from flask import Flask, request
 from niko import SenderTelegram
+from dotenv import load_dotenv
 import datetime
 import os
 import json
 
 app = Flask(__name__)
 app.secret_key = os.urandom(12)
+load_dotenv()
 
 @app.route("/kirimpesan/<id>/<pesan>", methods=['GET'])
 async def kirimpesan(id,pesan):
     waktu = datetime.datetime.now().strftime("'%d-%b-%Y %H:%M:%S'")
-    sender = SenderTelegram(target=id)
-    await sender.client.start()
-    await sender.client.connect() 
-    teleId = await sender.getId()
-    if isinstance(teleId[0], str):
-        await sender.client.disconnect()    
-        result = {"status":"error", "message_lenght":len(pesan), "target":id,"message": "username not found", "time_request": waktu}
-        return result, 400                
-    else: 
-        try:
-            for item in teleId:
-                await sender.sendMessage(item,pesan)
-        except:
-            await sender.client.disconnect()    
-            result = {"status":"error", "message_lenght":len(pesan), "target":id,"message": "something wrong", "time_request": waktu}
-            return result, 500                     
+    sender = SenderTelegram()
 
-    await sender.client.disconnect()    
-    result = {"status":"success", "message_lenght":len(pesan), "target":id,"message": "message sent", "time_request": waktu}
-    return result, 200    
-
+    await sender.initTelegramClient()
     
-   
+    try: 
+        await sender.sendMessage(id,pesan)
+        await sender.client.disconnect()  
+        result = {"status":"success", "message_length":len(pesan), "target":id,"message": "message sent", "time_request": waktu}
+        return result, 200          
+    except Exception as a:
+        print("test exec")
+        await sender.client.disconnect()  
+        error_code = 500
+        err_msg = str(a)
+        if err_msg == "username not found":
+            error_code = 400
+        
+        result = {"status":"error", "message_length":len(pesan), "target":id,"message": err_msg, "time_request": waktu}
+        return result, error_code
+
 
 @app.route("/kirimpesans", methods=['POST'])
 async def kirimpesans():
     waktu = datetime.datetime.now().strftime("'%d-%b-%Y %H:%M:%S'")
-    sender = SenderTelegram(target=request.json["usernames"])
+    sender = SenderTelegram()
+    usernames = request.json["usernames"]
     pesan = request.json["pesan"]
-    errorProses = []
-    await sender.client.start()
-    await sender.client.connect() 
+    errorProsesUsernames = []
+    errorProsesMessage = []
+    limit = False
+    
+    await sender.initTelegramClient()
+
     try:
-        teleId = await sender.getId()
-        for item in teleId:
-            if isinstance(item, str):
-                errorProses.append(item)
-            else:
+        for item in usernames:
+            try:
                 await sender.sendMessage(item,pesan)
-    except:
+            except Exception as e: 
+                if str(e) == "telegram api has been limited":
+                    limit = True
+
+                errorProsesUsernames.append(item)
+                errorProsesMessage.append({item:str(e)})
+
+    except Exception as hehe:
         await sender.client.disconnect()    
-        result = {"status":"error", "message_lenght":len(pesan),"target_lenght":len(request.json["usernames"]), "target":request.json["usernames"],"error_list":errorProses, "time_request": waktu}
+        result = {"status":"error", "message_length":len(pesan),"total_target":len(request.json["usernames"]), "target":request.json["usernames"],"usernames_error_list":errorProsesUsernames,"error_message": "something wrong","usersnames_error_messages":errorProsesMessage, "time_request": waktu}
         return result, 500        
     
     await sender.client.disconnect()
-   
-    result = {"status":"success", "message_lenght":len(pesan),"target_lenght":len(request.json["usernames"]), "target":request.json["usernames"],"error_list":errorProses, "time_request": waktu}
+
+    if len(usernames) == len(errorProsesUsernames) and limit:
+        result = {"status":"error", "message_length":len(pesan),"total_target":len(request.json["usernames"]), "target":request.json["usernames"],"usernames_error_list":errorProsesUsernames,"error_message": "telegram api has been limited", "usersnames_error_messages":errorProsesMessage, "time_request": waktu}
+        return result, 500
+
+    result = {"status":"error", "message_length":len(pesan),"total_target":len(request.json["usernames"]), "target":request.json["usernames"],"usernames_error_list":errorProsesUsernames,"error_message": None, "usersnames_error_messages":None, "time_request": waktu}
     return result, 200
 
 if __name__ == '__main__':
-    PORT = 5000
-    app.run(debug=False, host='0.0.0.0', port=PORT)
+    app.run(debug=False, host='0.0.0.0', port=os.getenv("PORT_APPS"))
